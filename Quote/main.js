1db87553d3fbaf5b5517938f8d97222fdc8be1af
jd1023 <juandrevdw1@gmail.com>
2017-12-27 13:15:47 +0200

    Added code generator

---
+++ Quote/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  getQuote();
+  $("#quote-btn").click(function(){
+    getQuote();
+   $("body").css('backgroundColor', hue);
+  });
+   $('#tweet').on('click', tweetFunc);
+   $("body").css('backgroundColor', hue);
+});
+
+function getQuote() {
+  $.ajax({
+    type: "POST",
+    crossDomain: true,
+    url: "https://api.forismatic.com/api/1.0/",
+    data: {
+      method: "getQuote",
+      format: "jsonp",
+      lang: "en"
+    },
+    dataType: "jsonp",
+    jsonp: "jsonp",
+    jsonpCallback: "parseQuote"
+  });
+
+
+}
+
+function parseQuote(response) {
+  $('#quote').text(response.quoteText);
+  $('#author').text(response.quoteAuthor);
+  var text = "'" + response.quoteText + "' - " + response.quoteAuthor;
+
+}
+
+function hue() {
+  return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+}
+
+
+
+function tweet(quote) {
+  window.open('https://twitter.com/intent/tweet?hashtags=&text='   + encodeURIComponent(quote));
+}
+function tweetFunc() {
+  tweet($(quote).text());
+}
